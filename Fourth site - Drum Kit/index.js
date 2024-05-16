0f6a3d9aed68f9d445398ecf13360d134116149f
David Denis <daviddenis656@gmail.com>
2024-05-16 23:02:59 +0300

    Added the modifications for the drum kit and the new Game app Simon

--- Fourth site - Drum Kit/index.js
+++ Fourth site - Drum Kit/index.js
@@ -4,12 +4,13 @@ for (var i = 0; i < loopNumber; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     })
 }
 
 document.addEventListener("keypress", function (event) {
     makeSound(event.key);
-
+    buttonAnimation(event.key);
 
 })
 
@@ -47,6 +48,15 @@ function makeSound(key) {
             break;
 
         default:
-            console.log(Key);
+            console.log(key);
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
